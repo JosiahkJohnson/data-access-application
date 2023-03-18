65feb765c8495bb885764126773f1c51f2c4908f
Josiah <josiahkj123@gmail.com>
2023-03-18 01:54:36 -0500

    Created simple local application

---
+++ data-access-application/Controller.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.Configuration;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace data_access_application
+{
+    class Controller
+    {
+        string connection;
+        SqlConnection connect = null;
+
+        public Controller()
+        {
+            connection = "Server = LAPTOP-1HMCDI96\\SQLEXPRESS; " +
+                          "Trusted_Connection = true;" +
+                          "Database = Northwind;" +
+                          "User Instance = false;" +
+                          "Connection timeout = 30";
+        }
+        public Controller(string connect)
+        {
+            connection = connect;
+        }
+
+        public string getCustomerNumber()
+        {
+            int count = 0;
+            connect = new SqlConnection(connection);
+            connect.Open();
+
+            string Query = "SELECT COUNT(*) FROM customers;";
+            SqlCommand command = new SqlCommand(Query, connect);
+
+            try
+            {
+                count = Convert.ToInt32(command.ExecuteScalar());
+            }
+            catch(Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+
+            connect.Close();
+            return count.ToString();
+        }
+
+        public string getCustomerNames()
+        {
+            string names = "";
+            SqlDataReader reader;
+            connect = new SqlConnection(connection);
+            connect.Open();
+
+            string Query = "SELECT companyname FROM customers;";
+            SqlCommand command = new SqlCommand(Query, connect);
+            reader = command.ExecuteReader();
+            while(reader.Read())
+            {
+                try
+                {
+                    names = names + reader.GetValue(0) + "\n";
+                } catch (Exception exception)
+                {
+                    Console.WriteLine(exception.Message);
+                }
+            }
+
+            connect.Close();
+            return names.ToString();
+        }
+    }
+}
---
+++ data-access-application/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace data_access_application
+{
+    public partial class Form1 : Form
+    {
+
+        Controller database;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            database = new Controller("Server = LAPTOP-1HMCDI96\\SQLEXPRESS;" +
+                                      "Trusted_Connection = true;" +
+                                      "Database = NorthWind;" +
+                                      "User Instance = false;" +
+                                      "Connection timeout = 30");
+            MessageBox.Show("Connection info sent");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string count = database.getCustomerNumber();
+            MessageBox.Show(count + " Users");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string result = database.getCustomerNames();
+            MessageBox.Show(result);
+        }
+    }
+}
